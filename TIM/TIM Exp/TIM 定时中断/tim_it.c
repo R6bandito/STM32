c4080f7461732fbd7a010dc4699ec5ea02fa2c7f
R6bandito <144506399+R6bandito@users.noreply.github.com>
2025-07-14 12:07:31 +0800

    Update tim_it.c

--- TIM/TIM Exp/TIM 定时中断/tim_it.c
+++ TIM/TIM Exp/TIM 定时中断/tim_it.c
@@ -4,7 +4,7 @@ void TIM3_Init(void) {
   TIM_TimeBaseInitTypeDef TIM_InitStructure;
   NVIC_InitTypeDef NVIC_InitStructure;
 
-	TIM_InternalClockConfig(TIM3); // 内部时钟配置到TIM3时基单元
+  TIM_InternalClockConfig(TIM3); // 内部时钟配置到TIM3时基单元
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); // 使能TIM3时钟
   TIM_InitStructure.TIM_ClockDivision = TIM_CKD_DIV1; // 时钟分频
   TIM_InitStructure.TIM_CounterMode = TIM_CounterMode_Up; // 向上计数模式
@@ -12,7 +12,7 @@ void TIM3_Init(void) {
   TIM_InitStructure.TIM_Prescaler = 7200 - 1; // 计时1s
   TIM_InitStructure.TIM_RepetitionCounter = 0;
   TIM_TimeBaseInit(TIM3, &TIM_InitStructure); // 初始化TIM3
-	TIM_ClearFlag(TIM3, TIM_FLAG_Update);
+  TIM_ClearFlag(TIM3, TIM_FLAG_Update);
   TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE); // 使能TIM3更新中断
 
   NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn; // 中断通道3
